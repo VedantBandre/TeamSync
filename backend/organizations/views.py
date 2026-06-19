60d51859591208a7ba2448847f5183792a1002be
VedantBandre <vedantbandre33@gmail.com>
2026-06-19 18:36:55 +0200

    RBAC authorization

--- backend/organizations/views.py
+++ backend/organizations/views.py
@@ -1,13 +1,35 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Organization, Membership
 from .serializers import OrganizationSerializer, MembershipSerializer
 
 # Create your views here.
 class OrganizationViewSet(viewsets.ModelViewSet):
-    queryset = Organization.objects.all()
+    permission_classes = [IsAuthenticated]
     serializer_class = OrganizationSerializer
+    queryset = Organization.objects.all()
+    def get_queryset(self):
+        return Organization.objects.filter(
+            membership_user=self.request.user
+        )
+    
+    def perform_create(self, serializer):
+        organization = serializer.save()
+        Membership.objects.create(
+            user=self.request.user,
+            organization=organization,
+            role="ADMIN"
+        )
+
 
 class MembershipViewSet(viewsets.ModelViewSet):
-    queryset = Membership.objects.all()
+    permission_classes = [IsAuthenticated]
     serializer_class = MembershipSerializer
+    queryset = Membership.objects.all()
+    
+    def get_queryset(self):
+        return Membership.objects.filter(
+            user=self.request.user
+        )
+    
